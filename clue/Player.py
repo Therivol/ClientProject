985ba5c931c1feb609f9bfd639b8f629d3eaccec
Therivol <alex@barba.org>
2023-02-16 11:21:02 -0700

    idk

--- clue/Player.py
+++ clue/Player.py
@@ -1,14 +1,17 @@
 import pygame as p
 
+from pygame.math import Vector2
+
 
 class Player:
-    def __init__(self):
-        self.location = (0, 0)
-        self.color = (0, 0, 0)
-        self.room = ""
+    def __init__(self, name, location, color, room=""):
+        self.name = name
+        self.location = p.Vector2(location)
+        self.color = color
+        self.room = room
 
     def set_location(self, location):
-        self.location = location
+        self.location.update(location)
 
     def set_color(self, color):
         self.color = color
--- clue/scene/Board.py
+++ clue/scene/Board.py
@@ -5,6 +5,7 @@ from util.Settings import Settings
 from util.Input import Input
 from util.Scenes import Scenes
 from clue.scene.Scene import Scene
+from clue.Player import Player
 import pygame as p
 
 
@@ -21,6 +22,8 @@ class Board(Scene):
 
         self.TILE_SIZE = Settings.get("TILE SIZE")
 
+        self.add_player(Player("Player 1", (5, 1), (255, 0, 0)))
+
     def update(self):
         if Input.get_key_down(p.K_SPACE):
             print(random.randint(1, 6))
@@ -30,28 +33,51 @@ class Board(Scene):
 
     def get_surface(self):
         surf = p.Surface((Settings.get("RESOLUTION")))
-        surf.fill((0, 0, 0))
+        surf.fill((200, 200, 200))
 
         y = 0
         for row in self.board:
             x = 0
             for tile in row:
                 color = (0, 0, 0)
+
+                outline = (0, 0, 0)
+
                 if tile == 0:
                     color = (0, 0, 0)
                 elif tile == 1:
                     color = (200, 200, 200)
                 elif tile == 2:
                     color = (209, 185, 128)
+                    outline = (229, 205, 148)
                 elif tile == 3:
                     color = (125, 150, 125)
                 elif tile == 4:
                     color = (125, 125, 150)
-                x_pos, y_pos = x * self.TILE_SIZE, y * self.TILE_SIZE
-                p.draw.rect(surf, color, (x_pos, y_pos, self.TILE_SIZE, self.TILE_SIZE))
-                if tile == 2 or tile == 0:
-                    p.draw.rect(surf, (0, 0, 0), (x_pos, y_pos, self.TILE_SIZE, self.TILE_SIZE), 1)
+
+                if tile != 1:
+                    x_pos, y_pos = x * self.TILE_SIZE, y * self.TILE_SIZE
+                    p.draw.rect(surf, color, (x_pos, y_pos, self.TILE_SIZE, self.TILE_SIZE))
+
+                    p.draw.rect(surf, outline, (x_pos, y_pos, self.TILE_SIZE, self.TILE_SIZE), 1)
                 x += 1
             y += 1
 
+        self.draw_players(surf)
+
         return surf
+
+    def add_player(self, player):
+        self.players.append(player)
+
+    def remove_player(self, name):
+        self.players = [player for player in self.players if player.name != name]
+
+    def draw_players(self, surf):
+        for player in self.players:
+
+            p.draw.rect(surf, (0, 0, 0), p.Rect(player.location * self.TILE_SIZE, (self.TILE_SIZE, self.TILE_SIZE)))
+            p.draw.circle(surf, player.color, player.location * self.TILE_SIZE +
+                          (self.TILE_SIZE / 2, self.TILE_SIZE / 2), self.TILE_SIZE / 2)
+            p.draw.rect(surf, (0, 0, 0), p.Rect(player.location * self.TILE_SIZE, (self.TILE_SIZE, self.TILE_SIZE)), 2)
+
